e2a350eaefc852742dad0c1e38e8006f641310b1
Chris Bennett <chrispaulbennett@gmail.com>
2019-06-14 17:00:40 +0100

    Frames implemente into interface.

--- Interface.py
+++ Interface.py
@@ -6,49 +6,92 @@ from PIL import Image, ImageTk
 
 class UI(tk.Tk):
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
 
-        tk.Tk.__init__(self)
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, PageOne):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
         # Creates PhotoboothClass object
         self.core = PhotoBoothClass.PhotoBooth()
-
-        # Allows for JPEG file type to be used
-        self.screen_width = self.winfo_screenwidth()
-        self.screen_height = self.winfo_screenheight()
-        image = Image.open("background_image.jpg")
-        image = image.resize((int(self.screen_width), self.screen_height))
-        self.background_image = ImageTk.PhotoImage(image)
-
-        #self.background_image = tk.PhotoImage(file="background_image.jpg")
-
-        self.background_label = tk.Label(self, image=self.background_image)
-        #self.background_label.place(x=0, y=0, width=self.screen_width, height=self.screen_height)
-        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-
-
-
-        self.quit_button = tk.Button(self, text="Quit", justify="center", command=self.close_window)
-        self.quit_button.place(relx=0.5, rely=0.8)
-        self.start_button = tk.Button(self, text="Start", justify="center", command=self.core.main_use_case)
-        self.start_button.place(relx=0.5, rely=0.5)
         self.attributes('-fullscreen', True)
         self.title("Photo Booth")
-        self.mainloop()
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
 
     def close_window(self):
         self.destroy()
-
-    def print_test(self):
-        print("button clicked")
 
     def get_screen_size(self):
 
         print(self.screen_height)
         print(self.screen_width)
 
-app = UI()
 
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+        # Allows for JPEG file type to be used
+        self.screen_width = self.winfo_screenwidth()
+        self.screen_height = self.winfo_screenheight()
+        image = Image.open("background_image.jpg")
+        image = image.resize((int(self.screen_width), self.screen_height))
+        self.background_image = ImageTk.PhotoImage(image)
+        #self.background_image = tk.PhotoImage(file="background_image.jpg")
+
+        self.background_label = tk.Label(self, image=self.background_image)
+        #self.background_label.place(x=0, y=0, width=self.screen_width, height=self.screen_height)
+        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.quit_button = tk.Button(self, text="Quit", justify="center", command=lambda: controller.close_window())
+        self.quit_button.place(relx=0.5, rely=0.8)
+        self.start_button = tk.Button(self, text="Start", justify="center", command=lambda: controller.show_frame(PageOne))
+        self.start_button.place(relx=0.5, rely=0.5)
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+        # Allows for JPEG file type to be used
+        self.screen_width = self.winfo_screenwidth()
+        self.screen_height = self.winfo_screenheight()
+        image = Image.open("background_image.jpg")
+        image = image.resize((int(self.screen_width), self.screen_height))
+        self.background_image = ImageTk.PhotoImage(image)
+        #self.background_image = tk.PhotoImage(file="background_image.jpg")
+
+        self.background_label = tk.Label(self, image=self.background_image)
+        #self.background_label.place(x=0, y=0, width=self.screen_width, height=self.screen_height)
+        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.quit_button = tk.Button(self, text="Quit", justify="center", command=lambda: controller.close_window())
+        self.quit_button.place(relx=0.5, rely=0.8)
+        self.start_button = tk.Button(self, text="Start", justify="center", command=lambda: controller.show_frame(StartPage))
+        self.start_button.place(relx=0.5, rely=0.5)
+
+app = UI()
+app.mainloop()
 
 # window = tk.Tk()
 # window.title("Photo Booth")
